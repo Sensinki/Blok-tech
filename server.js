1772cdb2c654e3fa8b5dbf431c279e32b25af5db
senanurkorkutan <snnrkrk1@gmail.com>
2023-05-08 12:35:57 +0200

    :building_construction: eslint installeren

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-const express = require('express');
-const { engine } = require('express-handlebars');
+const express = require('express')
+const { engine } = require('express-handlebars')
+const app = express()
+const PORT = process.env.PORT || 3000
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
 
 app.get('/', (req, res) => {
-    res.render('home');
-});
+    res.render('home')
+})
 
 app.get('/about', (req, res) => {
-    res.render('about');
-});
+    res.render('about')
+})
 
 app.listen(PORT, () => {
     console.log('App running on port', PORT)
-});
+})
