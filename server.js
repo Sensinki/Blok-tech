9fba1f7dc2955eb5b40e120634d56a0913b488b8
senanurkorkutan <snnrkrk1@gmail.com>
2023-05-11 03:05:38 +0200

    ::art: layouts, title and 404

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ const { engine } = require('express-handlebars')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.engine('handlebars', engine())
+app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.set('views', './views')
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', { title: 'Home' })
 })
 
 app.get('/about', (req, res) => {
-    res.render('about')
+    res.render('about', { title: 'About' })
+})
+
+app.use((req, res, next) => {
+    res.status(404).send(
+        '<h1>Page not found on the server</h1>')
 })
 
 app.listen(PORT, () => {
